2ac706b8c7a036beac4c3d26b1e92b0dec9320a7
Ruben Tarne <ruben.tarne@gmail.com>
2019-05-06 09:43:11 +0200

    Implement rent payments for social housing..
    The rental payment is static, and calibrated to data of the Ministry of Housing on local authority weekly rental prices for the fiscal year 2013-14

--- src/main/java/housing/Household.java
+++ src/main/java/housing/Household.java
@@ -218,6 +218,15 @@ public class Household implements IHouseOwner {
 //        	//	System.out.println("MonthlyDisposableIncome is negative: " + monthlyDisposableIncome);
 //        	}
         }
+        // households in social housing pay rent equal to average local authority rent prices for England from 2013-14 
+    	// https://data.london.gov.uk/dataset/local-authority-average-rents
+        // = 82.44 pounds of WEEKLY rent 
+        // TODO this is yearly rental cost of 3,957.12 pounds per year and can therefore be higher than private rents
+        if(home==null) {
+        	monthlyDisposableIncome -= 4*82.44;
+        	// in case disposable income becomes negative, assume the government covering the rent, so households don't dissave
+        	if(monthlyDisposableIncome<0) monthlyDisposableIncome=0;
+        }
         monthlyDisposableIncome -= monthlyPayments;
         return monthlyDisposableIncome;
     }
